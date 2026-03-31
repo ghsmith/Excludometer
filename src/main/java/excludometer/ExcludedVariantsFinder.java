14b3097127512f1d2a243b1f26cec8cc53b6726e
Geoffrey H. Smith <geoffrey.hughes.smith@gmail.com>
2026-03-30 21:10:57 -0400

    Update ExcludedVariantsFinder.java

--- src/main/java/excludometer/ExcludedVariantsFinder.java
+++ src/main/java/excludometer/ExcludedVariantsFinder.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import java.util.logging.Logger;
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.core.NewCookie;
 import org.glassfish.jersey.apache.connector.ApacheConnectorProvider;
 import org.glassfish.jersey.client.ClientConfig;
 import org.glassfish.jersey.client.ClientProperties;
@@ -20,31 +21,40 @@ public class ExcludedVariantsFinder {
 
     static final Logger logger = Logger.getLogger(ExcludedVariantsFinder.class.getName());
 
+    NewCookie jSessionId = null;
+
     String u;
     String p;
     
     public Variant[] findByOrderId(String orderId) throws InterruptedException {
         
-        ClientConfig cc = new ClientConfig().connectorProvider(new ApacheConnectorProvider());  
-        cc.register(JacksonFeature.class);
-        cc.property(ClientProperties.REQUEST_ENTITY_PROCESSING, RequestEntityProcessing.BUFFERED);
-        Client client = ClientBuilder.newClient(cc);
-        client.register(HttpAuthenticationFeature.basic(u, p));
-
         // get session cookie for an interactive GO session
         {
-            client.target("https://goprod.eushc.org/clinical-app/workbench/permissions")
+            ClientConfig cc = new ClientConfig().connectorProvider(new ApacheConnectorProvider());  
+            cc.register(JacksonFeature.class);
+            cc.property(ClientProperties.REQUEST_ENTITY_PROCESSING, RequestEntityProcessing.BUFFERED);
+            Client client = ClientBuilder.newClient(cc);
+            client.register(HttpAuthenticationFeature.basic(u, p));
+            jSessionId = client.target("https://goprod.eushc.org/clinical-app/workbench/permissions")
                 .request()
-                .get();
+                .get()
+                .getCookies()
+                .get("JSESSIONID");
             Thread.sleep(100);
         }
         
         Variant[] variants;
 
+        ClientConfig cc = new ClientConfig().connectorProvider(new ApacheConnectorProvider());  
+        cc.register(JacksonFeature.class);
+        cc.property(ClientProperties.REQUEST_ENTITY_PROCESSING, RequestEntityProcessing.BUFFERED);
+        Client client = ClientBuilder.newClient(cc);
+
         // request excluded variants
         {
             variants = client.target(String.format("https://goprod.eushc.org/clinical-app/workbench/analyses/%s-Combo/SNV/excludedVariants", orderId))
                 .request()
+                .cookie(jSessionId)
                 .get(Variant[].class);
             Thread.sleep(100);
         }
@@ -55,6 +65,7 @@ public class ExcludedVariantsFinder {
             x++;
             VariantDTO variantDTO = client.target(String.format("https://goprod.eushc.org/clinical-app/reportedmuts?ref=%s&chromosome=%s&start=%s&alt=%s", variant.ref, variant.chromosome, variant.start, variant.alt))
                 .request()
+                .cookie(jSessionId)
                 .get(VariantDTO.class);
             Thread.sleep(100);
             for(ReportedDTO reportedDTO : variantDTO.reportedMuts) {
